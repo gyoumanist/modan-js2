1b2136bb4f7d47419a72ee7a519dfd9bd97e82d3
gyoumanist <93202305+gyoumanist@users.noreply.github.com>
2022-03-09 21:45:53 +0900

    javascriptの基礎完了

--- src/index.js
+++ src/index.js
@@ -183,3 +183,22 @@ const nameArr = ["田中", "山田", "じゃけえ"];
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = "1300";
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "超えていません";
+// };
+
+// console.log(checkSum(40, 50));
